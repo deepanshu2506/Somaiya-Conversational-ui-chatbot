6b01041f206858b33b518aca938aa6bda71c5cec
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-02-09 00:20:47 +0530

    added logging to grouping service

--- groupingService/index.js
+++ groupingService/index.js
@@ -2,6 +2,9 @@ var mysql = require('mysql');
 const util = require('util'); 
 const _ = require('lodash');
 const axios = require('axios');
+const fs = require('fs');
+var os = require("os");
+
 
 var config ={
   host     : 'localhost',
@@ -30,14 +33,14 @@ var processData = async () => {
     try {
         const rows = await db.query('Select * from other_questions where isReplied = 0');
         const questionData = _.map(rows, (obj) => obj.question);
-        const maxGroupNoQuery = await db.query('select max(groupNo) as max from other_questions');
+        const maxGroupNoQuery = await db.query('select max(groupNo) as max from other_questions where isReplied = 1');
         const max = maxGroupNoQuery[0].max;
 
         const groups = await axios.post('http://127.0.0.1:2000/api/rankings', { questions: questionData });
         const response = JSON.parse(groups.data);
         console.log(response);
 
-
+        let count = 0
         for (res in response) {
             for (question of response[res]) {
                 console.log((max + parseInt(res)))
@@ -49,7 +52,11 @@ var processData = async () => {
                     + '" and isReplied = 0');
             }
         }
+        const logEntry = 'Grouping Service run successfully at ' + new Date() + os.EOL;
+        fs.appendFileSync('log.txt', logEntry);
     } catch (err) {
+        const logEntry = 'Error in grouping service on' + new Date() + os.EOL+"DETAILS OF ERROR:"+ os.EOL + err;
+        fs.appendFileSync('log.txt', logEntry);
         console.log(err);
     } finally {
         await db.close();
